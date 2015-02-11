31df8911bb999fb27a8e4ab118eab24f235bd520
Motornyuk Sergey <tonnydexter@gmail.com>
2015-02-11 16:38:06 +0200

    user get org

--- ckanext/dfmp/plugin.py
+++ ckanext/dfmp/plugin.py
@@ -23,6 +23,7 @@ class DFMPPlugin(plugins.SingletonPlugin):
         'organization_add_user':organization_add_user,
         'organization_remove_user':organization_remove_user,
         'all_user_list':all_user_list,
+        'user_get_organization':user_get_organization,
       }
 
 
@@ -117,7 +118,7 @@ def _delete_user_test(context, data_dict):
 
 
 def create_organization(context, data_dict):
-  if not 'name' in data_dict: raise toolkit.ValidationError('Parameter "name" must be defined')
+  validate(data_dict, 'name')
   name = _transform_org_name(data_dict['name'])
   title = data_dict['name']
   org = toolkit.get_action('organization_create')(context, {'name':name,'title':title})
@@ -128,9 +129,8 @@ def all_organization_list(context, data_dict):
   orgs = [ [key, value['title']] for key, value in enumerate(toolkit.get_action('organization_list')(context, {'all_fields':True}), 1) ]
   return orgs
 
-@side_effect_free
 def organization_add_user(context, data_dict):
-  if not 'user' in data_dict or not 'organization' in data_dict: raise toolkit.ValidationError('Parameters "user" and "organization" must be defined')
+  validate(data_dict, 'user', 'organization')
   user = _user_by_apikey(context, data_dict['user']).first()
   user_current_org = _organization_from_list(user.get_groups())[0]
   if user_current_org:
@@ -139,17 +139,27 @@ def organization_add_user(context, data_dict):
   return True
 
 def organization_remove_user(context, data_dict):
-  if not 'user' in data_dict or not 'organization' in data_dict: raise toolkit.ValidationError('Parameters "user" and "organization" must be defined')
+  validate(data_dict, 'user', 'organization')
   toolkit.get_action('organization_member_delete')(context, {'id':_transform_org_name(data_dict['organization']),'username':_user_by_apikey(context, data_dict['user']).first().id})
   return True
 
 @side_effect_free
 def all_user_list(context, data_dict):
   U = context['model'].User
-  G = context['model'].Group
   users = [dict(name=user.name, api_key=user.apikey, organization=_organization_from_list(user.get_groups())[1] ) for user in context['session'].query(U).filter(~U.name.in_(['default', 'visitor', 'logged_in']), U.state!='deleted' ).all()]
 
   return users
+
+@side_effect_free
+def user_get_organization(context, data_dict):
+  validate(data_dict, 'user')
+  org = _organization_from_list( _user_by_apikey(context, data_dict['user']).first().get_groups() )[1]
+  return org
+
+def validate(data, *fields):
+  for field in fields:
+    if not field in data: 
+      raise toolkit.ValidationError('Parameter {%s} must be defined' % field)
 
 def _organization_from_list(groups):
   if not len(groups):
