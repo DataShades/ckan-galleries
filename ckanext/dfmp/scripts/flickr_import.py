6a7ef5d6b21f3da482f2c7a00721eae47ea7e3ae
Iaroslav Pereiaslovets <iaroslav1309@gmail.com>
2015-04-16 18:02:10 +0300

    hotfix flickr

--- ckanext/dfmp/scripts/flickr_import.py
+++ ckanext/dfmp/scripts/flickr_import.py
@@ -188,7 +188,7 @@ def flickr_group_pool_add_images_to_dataset(context, data):
 
   # We get the total number of photos in the pool here
   photos = flickr.groups.pools.getPhotos(group_id=datastore['id'], per_page=1, page=1)
-  log.warn(photos);
+
   rough_total = int(photos[u"photos"][u"total"])
 
   # counting the number of iterations
@@ -288,7 +288,6 @@ def flickr_group_pool_import(context, url):
 
   # Group lookup
   group = flickr.urls.lookupGroup(url=group_url)
-  log.warn(group)
   # log.info(group)
 
   # Harvesting images from the group if possible
