963b77f77315dc76c16beee0e8c1b30b96a68edc
Motornyuk Sergey <tonnydexter@gmail.com>
2015-04-20 17:17:08 +0300

    ACTGXCCDE-179 / Implemented individual actions

---
+++ ckanext/dfmp/fanstatic/js/dfmp_manage_flagged.js
@@ -0,0 +1,34 @@
+"use strict";
+
+ckan.module('dfmp-manage-flagged', function ($, _) {
+  return {
+    initialize: function (){
+      $.proxyAll(this, /_on/);
+      this.el.on(
+        'click', this._onClick);
+
+    },
+
+    _onClick: function(e){
+      var self = this;
+      this.el.find('i').addClass('in-progress');
+
+      $.ajax({
+        url:this.options.url,
+        method:'POST',
+        data:{
+          action:this.options.action,
+          assets:this.options.asset.slice(1),
+          res_id:this.options.res
+        },
+        success: function (data) {
+          window.location.reload();
+        },
+        error: function (error, data) {
+          alert(error.status + ' ' + error.statusText);
+        }
+      });
+    },
+
+  }
+})
