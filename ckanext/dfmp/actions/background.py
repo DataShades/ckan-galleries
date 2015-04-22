d942e2ed09ee184491ea0aacfa43b9333334810b
Motornyuk Sergey <tonnydexter@gmail.com>
2015-04-22 12:07:19 +0300

    ACTGXCCDE-131 / added action to update all flickr datasets

--- ckanext/dfmp/actions/background.py
+++ ckanext/dfmp/actions/background.py
@@ -14,35 +14,10 @@ from datetime import datetime
 import json
 from ckan import model
 from ckan.model.types import make_uuid
-
+from ckanext.dfmp.dfmp_solr import DFMPSearchQuery
 from ckan.lib.dictization.model_dictize import resource_dictize
-
+import ckanext.dfmp.scripts.flickr_import as flickr_import
 from pylons import config
-
-@side_effect_free
-def celery_cleaning(context, data_dict):
-  task_id, celery_context = _prepare_celery(context, data_dict, 'clearing')
-  log.warn(task_id)
-  celery.send_task("dfmp.cleaning", args=[celery_context, data_dict], task_id=task_id)
-
-@side_effect_free
-def celery_getting_tweets(context, data_dict):
-  task_id, celery_context = _prepare_celery(context, data_dict, 'getting_tweets')
-  log.warn(task_id)
-  celery.send_task("dfmp.getting_tweets", args=[celery_context, data_dict], task_id=task_id)
-
-@side_effect_free
-def celery_streaming_tweets(context, data_dict):
-  task_id, celery_context = _prepare_celery(context, data_dict, 'streaming_tweets')
-  log.warn('task_id')
-  log.warn(task_id)
-  a = celery.send_task("dfmp.streaming_tweets", args=[celery_context, data_dict], task_id=task_id)
-  log.warn(a)
-
-def celery_revoke(context, data_dict):
-  task_id, celery_context = _prepare_celery(context, data_dict, 'revoke')
-  celery.send_task("dfmp.revoke_listener", args=[celery_context, data_dict], task_id=task_id)
-  return task_id
 
 def _prepare_celery(context, data_dict, task_type):
   task_id = str(uuid.uuid4())
@@ -72,16 +47,46 @@ def _prepare_celery(context, data_dict, task_type):
 
   return task_id, celery_context
 
+@side_effect_free
+def celery_cleaning(context, data_dict):
+  task_id, celery_context = _prepare_celery(context, data_dict, 'clearing')
+  celery.send_task("dfmp.cleaning", args=[celery_context, data_dict], task_id=task_id)
+
+@side_effect_free
+def celery_getting_tweets(context, data_dict):
+  task_id, celery_context = _prepare_celery(context, data_dict, 'getting_tweets')
+  celery.send_task("dfmp.getting_tweets", args=[celery_context, data_dict], task_id=task_id)
+
+@side_effect_free
+def celery_streaming_tweets(context, data_dict):
+  task_id, celery_context = _prepare_celery(context, data_dict, 'streaming_tweets')
+  a = celery.send_task("dfmp.streaming_tweets", args=[celery_context, data_dict], task_id=task_id)
+
+def celery_revoke(context, data_dict):
+  task_id, celery_context = _prepare_celery(context, data_dict, 'revoke')
+  celery.send_task("dfmp.revoke_listener", args=[celery_context, data_dict], task_id=task_id)
+  return task_id
 
 @side_effect_free
 def celery_flickr_import (context, data_dict):
   task_id, celery_context = _prepare_celery(context, data_dict, 'flickr_images')
-  log.warn(task_id)
   celery.send_task("dfmp.flickr_images", args=[celery_context, data_dict], task_id=task_id)
 
 @side_effect_free
 def celery_solr_indexing(context, data_dict):
   data_dict.update(solr_index=True)
   task_id, celery_context = _prepare_celery(context, data_dict, 'clearing')
-  log.warn(task_id)
   celery.send_task("dfmp.cleaning", args=[celery_context, data_dict], task_id=task_id)
+
+def dfmp_flickr_update(context, data_dict):
+  # gets all resource IDs
+  flickr_resources = DFMPSearchQuery()({
+    'q': ' +entity_type:asset +extras_source:flickr',
+    'fl': 'data_dict, id',
+    'facet.field': 'id',
+    'facet.limit': -1
+  })
+
+  # process resources one by one
+  for resource_id, number in flickr_resources['facets']['id'].items():
+    flickr_import.flickr_group_pool_resource_update (context, resource_id)
