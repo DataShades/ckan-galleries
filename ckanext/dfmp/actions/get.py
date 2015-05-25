cd696e9eeb9425b8be5d8df142608561319ae85d
Iaroslav Pereiaslovets <iaroslav1309@gmail.com>
2015-05-25 15:44:02 +0300

    log removal

--- ckanext/dfmp/actions/get.py
+++ ckanext/dfmp/actions/get.py
@@ -65,8 +65,7 @@ def dfmp_get_asset_list(context, data_dict):
 @side_effect_free
 def dfmp_get_thumbnail_url(context, data_dict):
   ''' Generates thumbnail and returns url. '''
-  log.warn(data_dict)
-  _validate(data_dict, 'image_url', 'width', 'height')
+   _validate(data_dict, 'image_url', 'width', 'height')
 
   import os
   import ckan.lib.uploader as uploader
