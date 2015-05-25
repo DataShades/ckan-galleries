427f3b78d43bc06b3498c0b78c0443aa0ed5aed7
Iaroslav Pereiaslovets <iaroslav1309@gmail.com>
2015-05-25 15:16:46 +0300

    image resing

--- ckanext/dfmp/plugin.py
+++ ckanext/dfmp/plugin.py
@@ -125,6 +125,11 @@ class DFMPPlugin(plugins.SingletonPlugin, toolkit.DefaultDatasetForm):
       controller='ckanext.dfmp.controller:DFMPController',
       action='create_record', ckan_icon='')
 
+    map.connect(
+      'get_thumbnail', '/thumb/{resolution}/{image}',
+      controller='ckanext.dfmp.controller:DFMPController',
+      action='get_thumbnail', ckan_icon='')
+
     return map
 
   def get_helpers(self):
@@ -138,6 +143,7 @@ class DFMPPlugin(plugins.SingletonPlugin, toolkit.DefaultDatasetForm):
       'dfmp_relationship':dfmp_relationship,
       'dfmp_nice_date':dfmp_nice_date,
       'dfmp_relative_time':dfmp_relative_time,
+      'dfmp_get_thumbnail':dfmp_get_thumbnail,
       }
 
   def update_config(self, config):
@@ -185,6 +191,7 @@ class DFMPPlugin(plugins.SingletonPlugin, toolkit.DefaultDatasetForm):
       'dfmp_get_asset':dfmp_get_asset,
       'dfmp_get_asset_list':dfmp_get_asset_list,
       'dfmp_update_asset':dfmp_update_asset,
+      'dfmp_get_thumbnail_url':dfmp_get_thumbnail_url
     }
 
 
